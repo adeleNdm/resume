943c6055b38d16ea57bc63bea5b4c3c75b335ac2
ayleen <adele.nademi@gmail.com>
2020-09-21 17:20:36 +0330

    Updates

---
+++ precache-manifest.7925ca30cee4f789429b04e189936983.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcc540ed2db56486cae8df8180663657",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "9977ed7c607ba813cf69",
+    "url": "/resume/static/css/main.1adeac45.chunk.css"
+  },
+  {
+    "revision": "1f3a142cc377b1872382",
+    "url": "/resume/static/js/2.87538b83.chunk.js"
+  },
+  {
+    "revision": "e46fc7d39878ee64e7b083a156855138",
+    "url": "/resume/static/js/2.87538b83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9977ed7c607ba813cf69",
+    "url": "/resume/static/js/main.066a517a.chunk.js"
+  },
+  {
+    "revision": "8c1d914a1677cea53979",
+    "url": "/resume/static/js/runtime-main.75d8c91e.js"
+  }
+]);
